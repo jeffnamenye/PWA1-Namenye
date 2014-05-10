17a504bf031a7547e838443fcefca30f6d25bb38
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-10 09:48:34 -0400

    commit numer 2

--- Week1/goal1_assign_duel1/js/main.js
+++ Week1/goal1_assign_duel1/js/main.js
@@ -1,2 +1,23 @@
 /***** Jeff Namenye 05/08/14 Duel 1****/
  
+//self-executing function
+(function(){
+
+console.log("Fight!!!");
+
+//player name
+
+var playerName = "Kabal";
+var playerName = "Kratos";
+
+//player damage
+
+var playerOneDamage = 20;
+var playerTwoDamage = 20;
+
+//player health
+
+var playerOneHealth = 100; 
+var playerTwoHealth = 100; 
+
+var round = 0;
