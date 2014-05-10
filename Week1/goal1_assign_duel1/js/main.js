a9512af7a3f9ea0a8eea06e50bb5197564184207
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-10 10:21:34 -0400

    commit 3

--- Week1/goal1_assign_duel1/js/main.js
+++ Week1/goal1_assign_duel1/js/main.js
@@ -1,23 +1,40 @@
 /***** Jeff Namenye 05/08/14 Duel 1****/
  
 //self-executing function
-(function(){
+	(function(){
 
 console.log("Fight!!!");
 
 //player name
 
-var playerName = "Kabal";
-var playerName = "Kratos";
+	var playerName = "Kabal";
+	var playerName = "Kratos";
 
 //player damage
 
-var playerOneDamage = 20;
-var playerTwoDamage = 20;
+	var playerOneDamage = 20;
+	var playerTwoDamage = 20;
 
 //player health
 
-var playerOneHealth = 100; 
-var playerTwoHealth = 100; 
+	var playerOneHealth = 100; 
+	var playerTwoHealth = 100; 
 
-var round = 0;
+	var round = 0;
+
+//fight function
+
+	function fight () {
+
+	console.log ("in the fight function");
+
+alert(playerOneName + ":" + playerOneHealth + "**Start**" + playerTwoName + ":" + 
+playerTwoHealth);
+
+	for(var i = 0; i<10; i==){
+
+//random formula is - Math.floor(Math.random()*(max-min)+min);
+
+	var minDamage1 = playerOneDamage * .5;
+	var minDamage2 = playerTwoDamage * .5;
+	
