370f542efcae0bcf72a3e5ee3dfd39171163ab75
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-11 17:50:20 -0400

    Debug

--- Week1/goal3_assign_debug_v2/js/search.js
+++ Week1/goal3_assign_debug_v2/js/search.js
@@ -8,49 +8,50 @@
 	;
 	
 	// Validates search query
-	var validqte == function(query){
+	var validate = function(query){
 		
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){
+		while(query.charAt(0) === " "){
 			query = query.substring(1, query.length);
 		};
 		while(query.charAt(query.length-1) === ""){
 			query = query.substring(0, query.length-1);
-		;
+        };
 		
 		// Check search length, must have 3 characters
 		if(query.length < 3){
-			alert("Your search query is too small, try again.);
+			alert("Your search query is too small, try again.");
 			
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
 			searchInput.focus();
 			return;
 		};
-		
+              console.log(alert)
 		search(query);
 	};
-	
+	         console.log(1)
+
 	// Finds search matches
-	var search = function(query)
+	var search = function(query){
 		
 		// split the user's search query string into an array
-		var queryArray = query.join(" ");
+		var queryArray = query.split(" ");
 		
 		// array to store matched results from database.js
 		var results = [];
 
 		// loop through each index of db array
 		for(var i=0, j=db.length; i<j; i++){
-		
+
 			// each db[i] is a single video item, each title ends with a pipe "|"
 			// save a lowercase variable of the video title
 			var dbTitleEnd = db[i].indexOf('|');
-			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+			var dbitem = db[i].toLocaleLowerCase().substring(0, dbTitleEnd);
 			
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
+				var qitem = queryArray[ii].toLowerCase();
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
@@ -58,19 +59,20 @@
 				if(compare !== -1){
 					results.push(db[i]);
 				};
-			;
-		;
-		
+            };
+        };
+		         console.log(results)
 		results.sort();
 		
 		// Check that matches were found, and run output functions
 		if(results.length = 0){
 			noMatch();
+
 		}else{
 			showMatches(results);
 		};
 	};
-	
+
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
 	var noMatch = function(){
 		var html = ''+
@@ -79,12 +81,12 @@
 		;
 		resultsDIV.innerHTML = html;
 	};
-	
+
 	// Put matches into page as paragraphs with anchors
 	var showMatches = function(results){
 		
 		// THE NEXT 4 LINES ARE CORRECT.
-		var html = '<p>Results</p>', 
+		var html = '<p>Results</p>',
 			title, 
 			url
 		;
@@ -95,7 +97,7 @@
 			// title of video ends with pipe
 			// pull the title's string using index numbers
 			titleEnd = results[i].indexOf('|');
-			title = results[i].subString(0, titleEnd);
+			title = results[i].substring(0, titleEnd);
 			
 			// pull the video url after the title
 			url = results[i].substring(results[i].indexOf('|')+1, results[i].length);
@@ -105,16 +107,16 @@
 		};
 		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
 	};
-	
+
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
 	document.forms[0].onsubmit = function(){
 		var query = searchInput.value;
-		validqte(query);
+		validate(query);
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
 		return false;
-	;
+    };
 
 })();
