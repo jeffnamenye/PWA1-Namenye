bf10d927fb35055b8e58b014261eee6d487db95a
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-16 17:58:40 -0400

    first commit

--- Week2/assign_guessing_game_Namenye_Jeff/js/main.js
+++ Week2/assign_guessing_game_Namenye_Jeff/js/main.js
@@ -8,7 +8,24 @@
  */
 
 //Game variables
-(function ()
+(function () {
+
+    //Game variables
+    (function () {
+
+//my variables
+        var guessMade = 0;
+        var guessRemain = 0;
+        var playerGuess = 0;
+
+        // my html id and tag set up in my DOM
+        var dom = {
+            input: document.querySelector("#input"),
+            output: document.querySelector("#output"),
+            button: document.querySelector("button")
+        };
 
 
-})();
+
+
+    })();
