5a640be27b9f04f88c02a9b4b59844657dc028c1
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-29 06:54:29 -0400

    start of my main

--- Week4/goal7_assign_obj_to_prototypes/js/main.js
+++ Week4/goal7_assign_obj_to_prototypes/js/main.js
@@ -5,3 +5,26 @@
  * Jeffrey Namenye 5/29/14
  */
 
+(function(){
+
+
+    //my variables
+    var numOfPeople = 3;
+    var people = [];
+    var names = ["Jeff","Jenna","Anthony","Gussie","Nick" ];
+    var interval;
+
+    //my loop for the objects
+
+    for (var i = 0; i <numOfPeople; i++) {
+
+        var personIndex = Math.floor(Math.random() * names.length);
+
+        var person = new Person(names [personIndex], i+1);
+
+
+    }
+
+
+
+)};
