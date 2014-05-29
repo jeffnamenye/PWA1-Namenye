312f965e9ad214fc12dab8f23efcb2ef48443c3f
Jeff Namenye <jeffnamenye@hotmail.com>
2014-05-29 06:12:39 -0400

    Code for blue print

--- Week4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
+++ Week4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
@@ -1,6 +1,34 @@
 /**
  * Created by the JavaScript Development Team
- * Class: PWA
+ * Class: PWA1
  * Goal: Goal7
  * Jeffrey Namenye 5/29/14
  */
+(function(){
+
+     window.Person=Person;
+
+     //shows my job choices and action that they will do
+    Person.jobs =("Chef","Electronic Tech","Student","Artist");
+    Person.actions = ("Cooking","Fixing","Studying","Painting");
+
+    function Person(name,row){
+        console.log("Person Created: ",name);
+
+        this.name = name;
+
+        //sets up for a random person
+        this.action = Person.action[Math.floor((Math.floor() * Person.action.length)];
+
+
+        this
+    }
+
+
+
+
+
+
+
+
+})();
